f1ce05aa8ee070712ba1ae466316465a0c00ea41
Mohd Razi Abbas <md.raziabbas2006@gmail.com>
2026-03-03 14:02:04 +0530

    added fundamental folder

---
+++ Fundamentals/operation.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter a no";
+    cin>>a;
+    cout<<"Enter second no";
+    cin>>b;
+
+   int sum = a+b;
+   int diff = a-b;
+   int prod = a*b;
+   int div = a/b;
+
+   cout<<sum<<endl;
+   cout<<diff<<endl;
+   cout<<prod<<endl;
+   cout<<div<<endl;
+
+    return 0;
+
+}
